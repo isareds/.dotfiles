c500af0f661e7ed04ac83d8624cdeb0e056b01bf
isareds <ir.isacco.rossi@gmail.com>
2019-12-10 15:59:35 +0100

    add setup.sh script for vim configuration (install Vundle only)

--- setup.sh
+++ setup.sh
@@ -6,9 +6,11 @@ ls ~/.dotfiles -LRa -I.git
 select yn in "Yes" "No"; do
 	case $yn in
 	Yes)
+		ln -sfv ~/.dotfiles/git/__git_aliases ~
 		ln -sfv ~/.dotfiles/git/.gitconfig ~
 		ln -svf ~/.dotfiles/vim/.vimrc ~
 		ln -svf ~/.dotfiles/bash/.bash_profile ~
+		~/.dotfiles/vim/setup.sh
 		source ~/.bashrc
 		break
 		;;
