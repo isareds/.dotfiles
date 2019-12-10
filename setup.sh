4f9eff1111c37303e9c8be20df1fffe7b0d9f639
isareds <ir.isacco.rossi@gmail.com>
2019-12-10 14:36:05 +0100

    fix vimrc symlink

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ select yn in "Yes" "No"; do
     case $yn in
         Yes )
 		ln -sfv ~/.dotfiles/git/.gitconfig ~;
-		ln -svf ~/.dotfiles/vim/* ~;
+		ln -svf ~/.dotfiles/vim/.vimrc ~;
 		ln -svf ~/.dotfiles/bash/.bash_profile ~;
 		source ~/.bashrc
 		break;;
